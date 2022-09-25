8c82d1bbe66f0ce31fee183523e42089f0189e5c
Erdinch <erdinchorhan02@gmail.com>
2022-09-25 21:27:07 +0300

    cards added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import './App.css';
+import Cards from './components/Cards';
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="title-container">
+        <h1>Memory card</h1>
+      </div>
+      <Cards />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,66 @@
+const Cards = () => {
+  return (
+    <div className="cards-container">
+      <div className="first-row">
+        <div className="card-container">
+          <p>𐰀</p>
+        </div>
+        <div className="card-container">
+          <p>𐰁</p>
+        </div>
+        <div className="card-container">
+          <p>𐰂</p>
+        </div>
+        <div className="card-container">
+          <p>𐰃</p>
+        </div>
+        <div className="card-container">
+          <p>𐰄</p>
+        </div>
+        <div className="card-container">
+          <p>𐰅</p>
+        </div>
+        <div className="card-container">
+          <p>𐰆</p>
+        </div>
+        <div className="card-container">
+          <p>𐰇</p>
+        </div>
+        <div className="card-container">
+          <p>𐰈</p>
+        </div>
+      </div>
+      <div className="second-row">
+        <div className="card-container">
+          <p>𐰉</p>
+        </div>
+        <div className="card-container">
+          <p>𐰊</p>
+        </div>
+        <div className="card-container">
+          <p>𐰋</p>
+        </div>
+        <div className="card-container">
+          <p>𐰌</p>
+        </div>
+        <div className="card-container">
+          <p>𐰍</p>
+        </div>
+        <div className="card-container">
+          <p>𐰎</p>
+        </div>
+        <div className="card-container">
+          <p>𐰏</p>
+        </div>
+        <div className="card-container">
+          <p>𐰐</p>
+        </div>
+        <div className="card-container">
+          <p>𐰑</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
